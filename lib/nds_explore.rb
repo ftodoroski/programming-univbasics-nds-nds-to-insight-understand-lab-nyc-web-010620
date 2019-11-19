d4f7d45ab02e56f61e8ae5272f51e3c910442c95
Filip Todoroski <ftodoroski@ymail.com>
2019-11-19 03:12:26 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 require 'pp'
-require 'byebug'
+
 
 
 # Call the method directors_database to retrieve the NDS
@@ -10,9 +10,25 @@ def pretty_print_nds(nds)
   pp nds
 end
 
-#directors_database
-#pretty_print_nds(nds)
+
+pretty_print_nds(directors_database)
 
 def print_first_directors_movie_titles
-  # puts directors_database
+  i = 0
+  while i < directors_database[:movies].length
+    movie = directors_database[:movies][i][:title]
+    puts movie
+
+    i += 1
+  end
 end
+#
+# data_base = [
+#    {:name=>"Stephen Spielberg",
+#     :movies=>[ {:title=>"Jaws", :studio=>"Universal", :worldwide_gross=>260000000, :release_year=>1975},
+#                {:title=>"Close Encounters of the Third Kind", :studio=>"Columbia", :worldwide_gross=>135189114, :release_year=>1977},
+#                {:title=>"Raiders of the Lost Ark", :studio=>"Paramount", :worldwide_gross=>248159971, :release_year=>1981},
+#                {:title=>"E.T.the Extra-terrestrial", :studio=>"Universal", :worldwide_gross=>435110554, :release_year=>1982},
+#                {:title=>"Schindler's List", :studio=>"Universal", :worldwide_gross=>96898818, :release_year=>1993},
+#                {:title=>"Lincoln", :studio=>"Buena Vista", :worldwide_gross=>182207973, :release_year=>2012} ] }
+#   ]
